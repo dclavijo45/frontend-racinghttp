168ae2478627bbad4ad617ac6611f56e458a46a8
Juan Felipe Motato <juanmotato1 @g,ail.com>
2021-02-17 16:11:00 -0500

    Agregado logica para cambiar imagen en agregar producto, mejorada interfaz del chat

--- src/app/agregar-producto/agregar-producto.component.ts
+++ src/app/agregar-producto/agregar-producto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-producto',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarProductoComponent implements OnInit {
 
-  constructor() { }
+  form:FormGroup;
+  reader = new FileReader();
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      nombre: ['', Validators.required],
+      referencia: ['', Validators.required],
+      precio: ['', Validators.required],
+      cantidad: ['', Validators.required]
+    });
+  }
+  Validar(){
+    if (this.form.valid) {
+      console.log("Form valid!")
+      let data = {
+        correo: this.form.value.correo,
+        password: this.form.value.password
+      }
+    }else{
+      console.log("Form Invalid!");
+    }
+  }
+
+  log(event, imgCurrent){
+    try {
+      if (event.type == "image/jpeg" || event.type == "image/png") {
+    this.reader.readAsDataURL(event);
+    this.reader.onload =  () => {
+      imgCurrent.src = this.reader.result;
+    }
+      }else{
+        console.log(`Type file: ${event.type}`);
+
+      }
+
+    } catch (error) {
+      console.error("Error when change img")
+    }
+
   }
 
 }
